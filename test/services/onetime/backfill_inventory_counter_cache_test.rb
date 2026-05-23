168a964a67c64209c917e5321569e4a56f4279c2
Sahil Lavingia <sahil@gumroad.com>
2026-05-23 14:25:16 -0400

    test: skip-batch drain spec/services b-* (9 specs)

---
+++ test/services/onetime/backfill_inventory_counter_cache_test.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class OnetimeBackfillInventoryCounterCacheTest < ActiveSupport::TestCase
+  test "TODO: migrate spec/services/onetime/backfill_inventory_counter_cache_spec.rb (28 FB refs)" do
+    skip "Awaiting fixtures migration"
+  end
+end
---
+++ test/services/purchase/associate_bundle_product_level_gift_service_test.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class PurchaseAssociateBundleProductLevelGiftServiceTest < ActiveSupport::TestCase
+  test "TODO: migrate spec/services/purchase/associate_bundle_product_level_gift_service_spec.rb (12 FB refs)" do
+    skip "Awaiting fixtures migration"
+  end
+end
